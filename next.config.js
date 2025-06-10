d9cd858be07f47225050ae3461c19f4d8713661f
Jmdec <infinitech.justin2024@gmail.com>
2025-06-10 10:26:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,17 +2,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  server: {
-    actions: {
-      bodySizeLimit: "10mb", // Adjust this limit as per your needs
-    },
-  },
   async redirects() {
     return [
       {
         source: "/",
         destination: "/admin",
-        permanent: true, // Use 'false' if you want a temporary redirect
+        permanent: true, // Set to false if you want a temporary redirect
       },
     ];
   },
@@ -20,7 +15,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*", // your Laravel API
+        destination: "https://infinitech-api5.site/api/:path*", // Updated Laravel API URL
       },
     ];
   },
